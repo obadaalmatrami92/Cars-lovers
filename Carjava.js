6c161958d66652fdf3bd27f80fac3cb812f178b4
obada almatrami <obada.matrami92@gmail.com>
2019-10-03 00:35:09 +0300

    add

---
+++ Carjava.js
@@ -0,0 +1,12 @@
+var driver = prompt("when the first human Drive a car ?");
+if(driver === "obada"){
+  confirm("correct answer");
+}else{
+  confirm("sorry try again");
+}
+var driver = prompt("which is the Best cars company ?");
+if(driver === "BMW"){
+  alert("correct answer");
+}else{
+  alert("sorry try again");
+}
