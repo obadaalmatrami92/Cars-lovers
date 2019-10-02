49834ee7452eec41739cb6ee68925d8d55754e39
obada almatrami <obada.matrami92@gmail.com>
2019-10-03 01:36:52 +0300

    edit

--- Carjava.js
+++ Carjava.js
@@ -1,12 +1,33 @@
+
 var driver = prompt("when the first human Drive a car ?");
 if(driver === "obada"){
   confirm("correct answer");
 }else{
   confirm("sorry try again");
 }
-var driver = prompt("which is the Best cars company ?");
-if(driver === "BMW"){
+var driver4 = prompt("which is the Best cars company ?");
+if(driver4 === "BMW"){
   alert("correct answer");
 }else{
   alert("sorry try again");
 }
+
+
+
+
+
+
+
+var driver3 = prompt("please enter number = 92 ?");
+
+while (driver3 != 92 ){
+  driver3 = prompt("sorry enter the correct number which is 92 :");
+}
+if(driver3 === "92"){
+  alert("correct answer");
+}else{
+  alert("sorry try again");
+}
+
+
+
